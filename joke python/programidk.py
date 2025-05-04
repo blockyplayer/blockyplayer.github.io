d516104064ab99e60312833e42a24f9ba954e32c
Blocky_Player/mr_block <40656709+Blocky-Player@users.noreply.github.com>
2025-05-04 15:14:29 +1000

    Delete joke python directory

--- joke python/programidk.py
+++
@@ -1,4 +0,0 @@
-di = int(input("How many sides does the di have? "))
-
-for i in range(di):
-    
--- joke python/while.py
+++
@@ -1,4 +0,0 @@
-import webbrowser
-
-while 1 != 2:
-    webbrowser.open_new_tab("https://youtube.com")
