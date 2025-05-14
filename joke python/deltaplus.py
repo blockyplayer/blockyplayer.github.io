2ed1fc995f072932f9d6f54401495971c6fd10e5
Blocky_Player/mr_block <40656709+Blocky-Player@users.noreply.github.com>
2025-05-14 17:33:18 +1000

    Add files via upload

--- joke python/deltaplus.py
+++ joke python/deltaplus.py
@@ -1,43 +1,46 @@
 def linear():
-    y = input('Enter linear equation (y=mx+c): ')
+    fx = input('Enter linear equation (y=mx+c): ').lower()
+
     #testing if there is a value for c, if not it is 0
-    if y[-1] != 'x':
-        c = int(y[-1])
-    else:
+    if fx[-1] == 'x':
         c = 0
+    else:
+        c = int(fx.partition('x')[-1])
+
     #testing if there is a value for m, if not it is 1
-    if y[0] == 'x':
+    if fx[0] == 'x':
         m = 1
-    elif y[0] != 'x':
-        m = int(y[0])
+    elif fx[0] != 'x':
+        m = int(fx.partition('x')[0])
     else:
         m = int(input('Value for {y[0]}? '))
-    obj = '/'
-    if '-' not in y:
-        c = int(-c)
-        x = 5
-        if m >= 3:
-            obj = '|'
-        elif m < 3:
-            obj = '/'
-        for i in range(5):
-            y = int((m * x) + c)
-            print(' '*x, end='')
-            print(obj)
-            if m != 1:
-                for i in range(m-1):
-                    print('')
-            else:
-                print('\n')
-            x = x - 1
-        if c > 0:
-            for i in range(c):
-                print(' ')
-                print('- '*20,end='')
-                print('>')
-        elif c == 0:
-            print('- '*20,end='')
-            print('>')
 
+    print(f'm = {m}, c = {c}')
+
+    obj = 'O'
+
+    # if '-' not in fx:
+    #     c = int(-c)
+    x = 10
+    # if m >= 3:
+    #     obj = '|'
+    # elif m < 3:
+    #     obj = '/'
+    print('^')
+
+    while x > 1:
+        for i in range(m-1):#logic to leave space for obj
+            print('|') #move cursor downwards by m amount
+            y = round((m * x) + c)
+        print(f'|{' '*(x-2)}', end='')#-2 because there are two characters printed on the line
+        print(obj)
+        x = x - 1
+    # print(obj)
+    # for i in range(c):
+    #     print('|')
+    for i in range(c):
+        print('|') 
+    print(f'{'﹉'*7}>')
+        
 while True:
     linear()
