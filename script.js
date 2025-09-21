454c45b747eed091ca80fe1af8d1cc7c116da2e3
Blocky Player <146407432+blockyplayer@users.noreply.github.com>
2025-09-21 12:06:00 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function getInputValue() {
     const query = 'https://www.google.com/search?q=${inputValue}';
     document.location='https://www.google.com/search?q=', $;{inputValue}
 }
+
+const url = 'https://api.ipstack.com/check?access_key=20c69cfe46bda218d7180411032a148e';
+const options = {
+	method: 'GET'
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
